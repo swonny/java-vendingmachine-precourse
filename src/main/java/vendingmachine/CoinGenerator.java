c1717b72a8c2e25700127c45c5bd78c6c27a3d68
swonny <wonny921@gmail.com>
2022-12-15 21:45:06 +0900

    feat: 생성할 코인 개수 계산 기능 구현

--- src/main/java/vendingmachine/CoinGenerator.java
+++ src/main/java/vendingmachine/CoinGenerator.java
@@ -1,15 +1,33 @@
 package vendingmachine;
 
-import java.util.Collections;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
 import java.util.EnumMap;
+import java.util.List;
+
+import static vendingmachine.Coin.getAmountOfCoins;
 
 public class CoinGenerator {
     public EnumMap<Coin, Integer> generate(int vendingMachineMoney) {
-        // TODO : return값 변경
-        // TODO : 코인 목록 생성과 0 초기화를 위해 init() 추가
-        EnumMap<Coin, Integer> tmp = new EnumMap<>(Coin.class);
-        tmp.put(Coin.COIN_10, 1);
-        tmp.put(Coin.COIN_500, 1);
-        return tmp;
+        EnumMap<Coin, Integer> changes = initializedChanges();
+        List<Integer> amountOfCoins = Coin.getAmountOfCoins();
+        while (vendingMachineMoney > 0) {
+            // TODO : 메소드 분리 가능해보임
+            int coinAmount = Randoms.pickNumberInList(amountOfCoins);
+            vendingMachineMoney -= coinAmount;
+            Coin coin = Coin.getCoinByAmount(coinAmount);
+            // TODO : 해시맵 가져오지 않고 바로 value에서 작업할 수 있는 방법 찾아보기
+            changes.put(coin, changes.get(coin) + 1);
+        }
+        return changes;
+    }
+
+    // TODO : APplication과 중복 -> 합치던가 클래스 하나 만들던가 하기
+    private static EnumMap<Coin, Integer> initializedChanges() {
+        EnumMap<Coin, Integer> changes = new EnumMap<>(Coin.class);
+        Arrays.stream(Coin.values())
+                .forEach(coin -> changes.put(coin, 0));
+        return changes;
     }
 }
